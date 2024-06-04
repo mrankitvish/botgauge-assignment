984699acdab73701a3dc18aeae80ab174db010fc
mrankitvish <ankitvishwakarma@gmail.com>
2024-06-04 14:55:17 +0530

    Provided Secrets2

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@
 provider "aws" {
   region = "us-west-2"
 
-  access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
-  secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
+  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
 }
 
 # Create an IAM role for the EKS cluster
